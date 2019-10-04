2478cec525a04beade8e1c5d9359277e74cfacd9
halilgezer <55877853+halilgezer@users.noreply.github.com>
2019-10-04 14:18:22 +0300

    4 işlem hesap makinesi.py

---
+++ Basit 4 işlem hesap makinesi.py
@@ -0,0 +1,14 @@
+a = input("İlk sayıyı giriniz : ")
+b = input ("İkinci sayıyı giriniz : ")
+
+a = int(a)
+b = int(b)
+
+print("Toplam sonucu : " , (a+b) )
+
+print ("Fark sonucu : " , (a-b) )
+
+print ("Çarpım sonucu: " , (a*b ) )
+
+print ("Bölme sonucu: " , (a//b ))
+
